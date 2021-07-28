027c602645b1b144151aaf60065025eada51da54
lakhara <siddharth.lakhara@gmail.com>
2021-07-28 21:37:56 +0530

    Listen to application events with event listeners

--- console/src/main/java/org/lakhara/console/ConsoleNumberGuess.java
+++ console/src/main/java/org/lakhara/console/ConsoleNumberGuess.java
@@ -4,16 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
 
 //    == Constants ==
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        log.info("Container ready for use.");
+    @EventListener
+    public void start(ContextRefreshedEvent event) {
+        log.info("start() --> Container ready for use.");
     }
 }
