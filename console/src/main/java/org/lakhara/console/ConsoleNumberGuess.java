9276491f21d1d0979a2404d686b16a9ee3d7a1ab
lakhara <siddharth.lakhara@gmail.com>
2021-07-28 21:33:08 +0530

    Listen to application event using interface

---
+++ console/src/main/java/org/lakhara/console/ConsoleNumberGuess.java
@@ -0,0 +1,19 @@
+package org.lakhara.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+
+//    == Constants ==
+    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        log.info("Container ready for use.");
+    }
+}
